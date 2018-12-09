381d117ba7d3e7b5d25e0f742628ce7647ddc288
Justin Mathew <justinmamathew@capgemini.com>
2018-12-09 14:25:34 +0530

    Enabled -d with docker-compose up to run in backgroud

--- ubuntuXenial/bootstrap.sh
+++ ubuntuXenial/bootstrap.sh
@@ -7,20 +7,27 @@ if ! [ -L /var/www ]; then
   ln -fs /vagrant /var/www
 fi
 
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+
 echo STARTING TO SET UP DOCKER CONTAINERS
 echo ==========================================
-sudo -i
+sudo su
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
 apt-get install docker.io -y
 echo bringig up Elasticsearch.. 
 cd  /vagrant/docker-files/dockerES
-docker-compose up
+docker-compose up -d 
 
 echo bringig up Redis.. 
 cd /vagrant/docker-files/dockerRedis
-docker-compose up
+docker-compose up -d
 
 echo bringig up MariaDb.. 
 cd /vagrant/docker-files/dockerMaria
-docker-compose up
+docker-compose up -d
 
 
